da8e557e3b543c8e5031f0ed89bbdaad45b0fe30
gmonroyrdz <gmonroy@cua.uam.mx>
2025-11-04 10:49:49 -0600

    GUI

--- gestor_empleados/src/App.java
+++ gestor_empleados/src/App.java
@@ -1,34 +1,11 @@
 
-import business.DepartamentoService;
-import dal.entity.Empleado;
-import java.util.List;
+import javax.swing.SwingUtilities;
+import pl.GUI;
 
 
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        DepartamentoService service = new DepartamentoService();
-        List<Empleado> empleados = service.searchById(100);
-        System.out.println("==============\n");
-
-        empleados = service.searchById(1);
-        for(Empleado e: empleados){
-            System.out.println(e.toString());
-        }
-
-        System.out.println("==============\n");
-
-        empleados = service.searchById(2);
-        for(Empleado e: empleados){
-            System.out.println(e.toString());
-        }
-
-        System.out.println("==============\n");
-
-        empleados = service.searchById(3);
-        for(Empleado e: empleados){
-            System.out.println(e.toString());
-        }
-        
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new GUI().createAndShowGui());
     }
 }
